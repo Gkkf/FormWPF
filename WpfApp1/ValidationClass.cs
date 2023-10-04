7e3fa7158efadb457545c4c4d98868c4982068f8
Gkkf <91417618+Gkkf@users.noreply.github.com>
2023-10-04 09:44:08 +0200

    Update ValidationClass.cs

--- WpfApp1/ValidationClass.cs
+++ WpfApp1/ValidationClass.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,13 +12,27 @@ namespace WpfApp1
 {
     public class ValidationClass : ValidationRule
     {
-        public static string toValidate = "";
+        public string toValidate { get; set; }
+
+        public ValidationClass()
+        {
+        }
 
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
-        {
-            if (value != null)
-                MessageBox.Show("Lipa");
-            return new ValidationResult(false, "Check");
+        { 
+            try
+            {
+                Regex regex = new Regex("[A-Z][a-z]+");
+                if (!regex.IsMatch((string)value))
+                    return new ValidationResult(false, $"Błędnie wpisane dane.");
+
+            }
+            catch (Exception e)
+            {
+                return new ValidationResult(false, $"Błąd: {e.Message}");
+            }
+
+            return ValidationResult.ValidResult;
         }
     }
 }

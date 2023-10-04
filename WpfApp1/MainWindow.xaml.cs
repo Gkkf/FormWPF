18e169a38584ae508c231dffd9d6a8166e9d4cdf
Gkkf <gkkf1@HP-GKKF>
2023-10-04 09:13:41 +0200

    Dodaj pliki projektów.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private string Plec()
+        {
+            string plec = "Mężczyzna";
+            if (m.IsChecked == true)
+            {
+                plec = "Mężczyzna";
+            }
+            else if (k.IsChecked == true)
+            {
+                plec = "Kobieta";
+            }
+            return plec;
+        }
+
+        private string Zainteresowania()
+        {
+            string zainteresowania = "";
+            
+            if (komputer.IsChecked == true)
+            {
+                zainteresowania = zainteresowania + "\n " + komputer.Content;
+            }
+
+            if (sport.IsChecked == true)
+            {
+                zainteresowania = zainteresowania + "\n " + sport.Content;
+            }
+
+            if (ksiazki.IsChecked == true)
+            {
+                zainteresowania = zainteresowania + "\n " + ksiazki.Content;
+            }
+
+            if (programowanie.IsChecked == true)
+            {
+                zainteresowania = zainteresowania + "\n " + programowanie.Content;
+            }
+
+            return zainteresowania;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.DefaultExt = ".jpg";
+
+            fileDialog.ShowDialog();
+            try
+            {
+                string imie = i.Text;
+                string nazwisko = n.Text;
+                string data = d.Text;
+
+                string zain = Zainteresowania();
+
+                if(zain == "") 
+                {
+                    zain = " \n Brak zainteresowań";
+                }
+
+                if(imie != String.Empty && nazwisko != String.Empty && data != String.Empty)
+                {
+                    label.Content = $" Imie: {imie} \n\r Nazwisko: {nazwisko} \n\r Data urodzenia: {data} \n\r Płeć: {Plec()} \n\r Zainteresowania: {zain}";
+                    img.Source = new BitmapImage(new Uri(System.IO.Path.GetFullPath(fileDialog.FileName), UriKind.RelativeOrAbsolute));
+                }
+                else
+                {
+                    label.Content = "Wypełnij wszystkie wymagane pola!";
+                }
+            }
+            catch(Exception ex)
+            {
+                label.Content = "Błędny format lub nie znaleziono pliku!";
+            }
+        }
+    }
+}
---
+++ WpfApp1/ValidationClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    public class ValidationClass : ValidationRule
+    {
+        public static string toValidate = "";
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value != null)
+                MessageBox.Show("Lipa");
+            return new ValidationResult(false, "Check");
+        }
+    }
+}
